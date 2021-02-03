93577cfdfc53306dbde923a7bd7a6bdcea997003
johnyworld <johnyworld@naver.com>
2021-02-03 09:59:58 +0900

    기본 자료형

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+void main() {
+  print('hello world');
+
+  // Boolean
+  bool visible = true;
+  print(visible);
+
+  // 정수
+  int num = 12 ~/ 5; // int 형에서는 나누기가 먹히지 않고, 몫만 구하는 ~/ 만 사용할 수 있다.
+  print(num);
+
+  // 소수
+  double num2 = 3.14 / 2;
+  print(num2);
+
+  // 문자열
+  String name = '홍길동';
+  print(name);
+
+  // 배열
+  List ages = [10, 11, 12];
+  print(ages[0]);
+
+  // 오브젝트
+  Map person = {'name': name, 'age': ages[1]};
+  print(person);
+}
